6e192c9ebe6dd5323962dbccdd93749a981aa4cc
Maciej <maciej.zakrzewski@zohomail.eu>
2021-07-20 21:45:10 +0200

    avg value calculated

--- src/main/java/com/pjatk/nbp/controller/NbpController.java
+++ src/main/java/com/pjatk/nbp/controller/NbpController.java
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/zloto")
+@RequestMapping("/gold")
 public class NbpController {
     private final NbpService nbpService;
 
@@ -14,11 +14,28 @@ public class NbpController {
         this.nbpService = nbpService;
     }
 
-    @PostMapping("")
-    ResponseEntity<String> getByDate(@RequestBody Query query){
+
+    //Zwraca query w formie stringa
+    @GetMapping("")
+    ResponseEntity<String> gold(@RequestBody Query query){
         nbpService.addQuery(query);
         System.out.println(query);
         return ResponseEntity.ok(query.toString());
     }
+
+    //Po prostu zwraca aktualną cenę złota
+    @GetMapping("/query/")
+    ResponseEntity<String> goldQuery(){
+        return ResponseEntity.ok(nbpService.goldQuery());
+    }
+
+
+    //Zwraca średnią cenę złota z podanego okresu
+    @PostMapping("/query/average")
+    ResponseEntity<String> averageGoldQuery(@RequestBody Query query){
+        return ResponseEntity.ok(nbpService.averageGoldQuery(query));
+    }
+
+
 
 }
--- src/main/java/com/pjatk/nbp/service/NbpService.java
+++ src/main/java/com/pjatk/nbp/service/NbpService.java
@@ -2,17 +2,67 @@ package com.pjatk.nbp.service;
 
 import com.pjatk.nbp.model.Query;
 import com.pjatk.nbp.repository.NbpRepository;
+import org.json.JSONObject;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class NbpService {
+    String url = "http://api.nbp.pl/api/cenyzlota/";
     private final NbpRepository nbpRepository;
+    private final RestTemplate restTemplate;
 
     public NbpService(NbpRepository nbpRepository) {
-        this.nbpRepository = nbpRepository;
-        addQueries();
 
+        this.nbpRepository = nbpRepository;
+        this.restTemplate = new RestTemplate();
+        addQueries();
     }
+
+    public String goldQuery(){
+        ResponseEntity<String> responseEntity = restTemplate.getForEntity(
+                url, String.class);
+
+        return responseEntity.getBody();
+    }
+
+    public String averageGoldQuery(Query query){
+        ArrayList<JSONObject> jsonObjects = null;
+        ResponseEntity<String> responseEntity = restTemplate.getForEntity(
+                url
+                        + query.getDateFrom() + "/"
+                        + query.getDateTo()
+                , String.class);
+
+        if(responseEntity.getStatusCodeValue() == 200 && responseEntity.hasBody()){
+            jsonObjects = getJsonObjects(responseEntity.getBody());
+        }
+
+        List<String> strings = jsonObjects.stream().map(elem -> elem.get("cena").toString()).collect(Collectors.toList());
+        double avg = strings.stream().mapToDouble(Double::parseDouble).sum() / jsonObjects.size();
+        System.out.println(avg);
+
+        return responseEntity.getBody();
+    }
+
+
+    public ArrayList<JSONObject> getJsonObjects(String body){
+        body = body.substring(1, body.length()-1).replace("},{", "},,{");
+        String[] strings = body.split(",,");
+        ArrayList<JSONObject> jsonObjects = new ArrayList<>();
+        for (String s:
+                strings) {
+            jsonObjects.add(new JSONObject(s));
+        }
+        return jsonObjects;
+    }
+
 
     public void addQuery(Query query){
         query.setDateNow();
